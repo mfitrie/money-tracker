f8b18a46cadda54dff3655ccdc5ae4d0862d839c
fitrie <mfitrie78@gmail.com>
2026-01-02 22:11:59 +0800

    feat(frontend): update code

--- frontend/components/other-component/appsidebar.tsx
+++ frontend/components/other-component/appsidebar.tsx
@@ -1,20 +1,56 @@
+import { Calendar, Home, Inbox, LayoutDashboard, Search, Settings, User } from "lucide-react"
+
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarHeader,
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar"
 
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Dashboard",
+    url: "#",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Account",
+    url: "#",
+    icon: User,
+  },
+]
+
 export function AppSidebar() {
-    return (
-        <Sidebar>
-            <SidebarHeader />
-            <SidebarContent>
-                <SidebarGroup />
-                <SidebarGroup />
-            </SidebarContent>
-            <SidebarFooter />
-        </Sidebar>
-    )
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
 }
--- frontend/components/other-component/navbar.tsx
+++ frontend/components/other-component/navbar.tsx
@@ -1,30 +1,19 @@
 "use client"
 
-import { MenuIcon, SearchIcon } from 'lucide-react'
-import { Button } from '@/components/ui/button'
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu'
 import { ModeToggle } from './mode-toggle'
+import { SidebarTrigger } from '../ui/sidebar'
 
 export default function Navbar() {
     return (
-        <header className='bg-background sticky top-0 z-50'>
-            <div className='mx-auto flex max-w-7xl items-center justify-between gap-8 px-4 py-7 sm:px-6'>
-                <div className='text-muted-foreground flex flex-1 items-center gap-8 font-medium md:justify-center lg:gap-16'>
-                    <span className='hover:text-primary max-md:hidden'>Money Tracker</span>
+        <header className='bg-background flex flex-col'>
+            <div className='flex items-center justify-between gap-8 px-4 py-7 sm:px-6'>
+                <div className='text-muted-foreground flex gap-8 font-medium justify-center'>
+                    <span className='hover:text-primary'>Money Tracker</span>
                 </div>
 
                 <div className='flex items-center gap-6'>
                     <ModeToggle />
-                    <Button variant='outline' size='icon'>
-                        <MenuIcon />
-                        <span className='sr-only'>Menu</span>
-                    </Button>
+                    <SidebarTrigger />
                 </div>
             </div>
         </header>
