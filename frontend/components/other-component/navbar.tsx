aeb8e83dd18a2780ccfbdad3c46f64a4f08a2845
fitrie <mfitrie78@gmail.com>
2026-01-03 16:11:54 +0800

    feat(backend): update code

--- frontend/components/other-component/navbar.tsx
+++ frontend/components/other-component/navbar.tsx
@@ -2,14 +2,18 @@
 
 import { ModeToggle } from './mode-toggle'
 import { SidebarTrigger } from '../ui/sidebar'
+import { Wallet } from 'lucide-react'
 
 export default function Navbar() {
     return (
         <header className='bg-background flex flex-col'>
             <div className='flex items-center justify-between gap-8 px-4 py-7 sm:px-6'>
-                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-                    Money Tracker
-                </h3>
+                <div className='flex flex-row items-center gap-2'>
+                    <Wallet/>
+                    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                        Money Tracker
+                    </h3>
+                </div>
                 <div className='flex items-center gap-6'>
                     <ModeToggle />
                     <SidebarTrigger className='cursor-pointer' />
