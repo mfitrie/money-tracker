b03d5fee45a52c9a4bf7d8810775b063d7f09a0f
fitrie <mfitrie78@gmail.com>
2026-01-11 12:59:14 +0800

    feat(frontend): make request dashboard

--- frontend/components/other-component/dashboard/today-chart.tsx
+++ frontend/components/other-component/dashboard/today-chart.tsx
@@ -17,6 +17,9 @@ import {
     type ChartConfig,
 } from "@/components/ui/chart"
 import { formatRMCurrency } from "@/utils/utils"
+import { getTodaysExpense, GetTodaysExpenseDTO } from "@/lib/queries/dashboard"
+import { useQuery } from "@tanstack/react-query"
+import { Spinner } from "@/components/ui/spinner"
 
 export const description = "A pie chart with a label"
 
@@ -65,6 +68,10 @@ export interface TodaysChartProps {
 
 
 export function TodaysChart(payload: TodaysChartProps) {
+    const { data: todaysExpenseData, isLoading: isLoadingTodaysExpense, error: errorTodaysExpense } = useQuery<GetTodaysExpenseDTO>({
+        queryKey: ['getTodaysExpense'],
+        queryFn: () => getTodaysExpense(),
+    });
 
     //* ------------------------------- Utils ------------------------------- *//
     function generateChartConfig(listData: TodaysChartProps['listData']): ChartConfig {
@@ -94,30 +101,42 @@ export function TodaysChart(payload: TodaysChartProps) {
 
 
     return (
-        <Card className="flex flex-col">
-            <CardHeader className="items-center pb-0">
-                <CardTitle>Today's Expenditure</CardTitle>
-                {/* <CardDescription>January - June 2024</CardDescription> */}
-            </CardHeader>
-            <CardContent className="flex-1 pb-0">
-                <ChartContainer
-                    config={config}
-                    className="[&_.recharts-pie-label-text]:fill-foreground mx-auto aspect-square max-h-[250px] pb-0"
-                >
-                    <PieChart>
-                        <ChartTooltip content={<ChartTooltipContent hideLabel />} />
-                        <Pie data={chartData} dataKey="spend" label nameKey="category" />
-                    </PieChart>
-                </ChartContainer>
-            </CardContent>
-            <CardFooter className="flex-col gap-2 text-sm">
-                <div className="flex items-center gap-2 leading-none font-light">
-                    You spend <span className="font-bold">{formatRMCurrency(5000, true)}</span> today!
-                </div>
-                {/* <div className="text-muted-foreground leading-none">
-                    Showing total visitors for the last 6 months
-                </div> */}
-            </CardFooter>
-        </Card>
+        <div>
+            {
+                errorTodaysExpense && (
+                    <span className='text-destructive'>{errorTodaysExpense.message}</span>
+                )
+            }
+            {
+                isLoadingTodaysExpense && (
+                    <Spinner />
+                )
+            }{
+                !isLoadingTodaysExpense && (
+                    <Card className="flex flex-col">
+                        <CardHeader className="items-center pb-0">
+                            <CardTitle>Today's Expenditure</CardTitle>
+                            {/* <CardDescription>January - June 2024</CardDescription> */}
+                        </CardHeader>
+                        <CardContent className="flex-1 pb-0">
+                            <ChartContainer
+                                config={config}
+                                className="[&_.recharts-pie-label-text]:fill-foreground mx-auto aspect-square max-h-[250px] pb-0"
+                            >
+                                <PieChart>
+                                    <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+                                    <Pie data={chartData} dataKey="spend" label nameKey="category" />
+                                </PieChart>
+                            </ChartContainer>
+                        </CardContent>
+                        <CardFooter className="flex-col gap-2 text-sm">
+                            <div className="flex items-center gap-2 leading-none font-light">
+                                You spend <span className="font-bold">{formatRMCurrency(todaysExpenseData?.data ?? 0, true)}</span> today!
+                            </div>
+                        </CardFooter>
+                    </Card>
+                )
+            }
+        </div>
     )
 }
---
+++ frontend/lib/queries/dashboard.ts
@@ -0,0 +1,17 @@
+// lib/queries/transactions.ts (or wherever you have it)
+import { ResponseGet } from "@/types/common-request.type";
+import { CreateTransactionDTO } from "@/validation/transaction";
+
+export interface GetTodaysExpenseDTO {
+    data: number,
+}
+
+
+
+export async function getTodaysExpense(): Promise<GetTodaysExpenseDTO> {
+    const res = await fetch('/api/dashboard');
+    if (!res.ok) {
+        throw new Error("Failed to fetch today's expense");
+    }
+    return res.json();
+}
