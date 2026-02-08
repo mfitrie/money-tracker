522b6f6c08fa8e978d03e5a5ae62ca641f188e38
Fitrie <mfitrie78@gmail.com>
2026-02-08 17:03:16 +0800

    Squashed commit of the following:
    commit 613ff1a7a3cf2947e60771130146ebfc0db06882
    Author: Fitrie <mfitrie78@gmail.com>
    Date:   Sun Feb 8 17:01:54 2026 +0800
    
        feat: add calendar for add transaction
    
    commit 0c4dada06dddbaef5f0a6553f50fc2dbb63d4414
    Author: Fitrie <mfitrie78@gmail.com>
    Date:   Sun Feb 8 16:22:03 2026 +0800
    
        feat: update get transaction
    
    commit e060311c044e47da79f6f9124208bc969b0b09cb
    Author: Fitrie <mfitrie78@gmail.com>
    Date:   Sun Feb 8 15:52:10 2026 +0800
    
        build: update docker compose

--- frontend/validation/transaction.ts
+++ frontend/validation/transaction.ts
@@ -1,11 +1,20 @@
 import { z } from "zod";
 
 export const CreateTransactionDTOSchema = z.object({
-    account_id: z.string().uuid(),
-    category_id: z.string(),
-    amount: z.number(),
-    type: z.enum(["expense", "income"]),
-    description: z.string().optional()
+    account_id: z.string({
+        error: "Required"
+    }).uuid(),
+    category_id: z.string({
+        error: "Required"
+    }),
+    amount: z.number({
+        error: "Required"
+    }),
+    type: z.enum(["expense", "income"], {
+        error: "Required"
+    }),
+    description: z.string().optional(),
+    transaction_date: z.string().datetime().optional()
 });
 
 export type CreateTransactionDTO = z.infer<typeof CreateTransactionDTOSchema>;
